08450d8dd26cdd817e74b6e3d485a72781698924
Joshua King <joshgking@gmail.com>
2023-10-22 18:57:35 -0700

    category and categories components and styling

---
+++ src/categories/categories.component.jsx
@@ -0,0 +1,14 @@
+import "./categories.styles.scss";
+import Category from "../category/category.component";
+
+const Categories = ({ categories }) => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <Category key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Categories;
