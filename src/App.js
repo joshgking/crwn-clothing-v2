403880c3c55ce2d5b260bd83f882246f26b403e0
Joshua King <joshgking@gmail.com>
2023-10-24 16:23:52 -0700

    navigation routes and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import Home from './routes/home/home.component';
+import SignIn from './routes/sign-in/sign-in.component';
+import Navigation from './routes/navigation/navigation.component';
 import { Route, Routes } from 'react-router-dom';
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
+      </Route>
     </Routes>
   );
 };
